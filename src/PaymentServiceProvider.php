615ace5a8cf42734a4dc750b0673b15a2a56cd79
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-14 23:04:47 +0330

    add: add PaymentServiceProvider service provider

---
+++ src/PaymentServiceProvider.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Modular\Payment;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/Config/payment.php', 'payment');
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPublishing();
+        $this->loadRequirements();
+
+        Payment::ApiRoutes();
+        Payment::WebRoutes();
+    }
+
+
+    /**
+     * load requirements section
+     */
+    private function loadRequirements()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/Database/Migrations');
+
+        $this->loadTranslationsFrom(__DIR__.'/Lang', 'payment');
+    }
+
+    /**
+     * register publishing file
+     */
+    private function registerPublishing()
+    {
+        $this->publishes([
+            __DIR__.'/Config/payment.php' => config_path('payment.php'),
+        ], 'payment-config');
+    }
+}
