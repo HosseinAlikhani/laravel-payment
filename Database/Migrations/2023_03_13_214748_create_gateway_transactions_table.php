2aecc9dbf12bf4ed1276b600b84fd9ebdca0310b
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-14 23:04:06 +0330

    add: add gateway_transactions transaction

---
+++ Database/Migrations/2023_03_13_214748_create_gateway_transactions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Modular\Payment\Models\GatewayTransaction;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gateway_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaction_id');
+            $table->integer('port');
+            $table->integer('amount');
+            $table->string('ref_id')->nullable();
+            $table->string('tracking_code');
+            $table->string('card_number')->nullable();
+            $table->enum('status',[
+                GatewayTransaction::STATUS_INIT,
+                GatewayTransaction::STATUS_SUCCEED,
+                GatewayTransaction::STATUS_FAILED
+            ]);
+            $table->string('ip')->nullable();
+            $table->text('description')->nullable();
+            $table->timestamp('payment_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gateway_transactions');
+    }
+};
