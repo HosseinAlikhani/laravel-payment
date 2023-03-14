5c718d5551ffd609e9b5f0a93ce5fde3867f3e57
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-03-14 23:05:59 +0330

    add: add GatewayTransaction model + add realation + add const status

---
+++ src/Models/GatewayTransaction.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Modular\Payment\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+class GatewayTransaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public const STATUS_INIT = 1;
+    public const STATUS_SUCCEED = 2;
+    public const STATUS_FAILED = 3;
+
+    /**
+     * generate tracking code
+     * @return string
+     */
+    public function generateTrackingCode(): string
+    {
+        return Str::random();
+    }
+
+    /**
+     * add logs relation has many
+     */
+    public function logs()
+    {
+        return $this->hasMany(GatewayTransactionLog::class, 'gateway_transaction_id', 'id');
+    }
+
+    /**
+     * transaction relation has one
+     */
+    public function transaction()
+    {
+        return $this->hasOne(Transaction::class, 'id', 'transaction_id');
+    }
+}
