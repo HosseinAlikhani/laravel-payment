023d6eed9fd5378eb30e0abfaad425ef67e4c5ee
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-07 21:47:14 +0330

    update: update PaymentController

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -2,19 +2,30 @@
 namespace D3CR33\Payment\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Modular\Infrastructure\Base\BaseController;
 use D3CR33\Payment\Services\PaymentService;
 
-class PaymentController extends BaseController
+class PaymentController
 {
+    /**
+     * store request
+     * @var Request
+     */
+    private Request $request;
+
     public function __construct(Request $request)
     {
         $this->request = $request;
     }
 
+    /**
+     * payment callback action
+     * @method GET
+     * @link payment/callback
+     */
     public function getPaymentCallback()
     {
         $response = app(PaymentService::class)->paymentCallback($this->request->all());
-        dd( $response );
+        //TODO must set payment view
+        return view('payment', compact('response'));
     }
 }
