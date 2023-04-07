e6142e03a995c06ec903102b17abc7b4823f39e4
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-07 21:38:16 +0330

    update: update payment config

--- Config/payment.php
+++ Config/payment.php
@@ -4,7 +4,7 @@ return [
     'ZARINPAL' => [
         'IS_TEST' => true,
 
-        'API_KEY'  =>  '',
+        'API_KEY'  =>  '1122334455667788998811223344556677',
 
         'SEND' => 'https://ir.zarinpal.com/pg/services/WebGate/wsdl',
         'GATE'  =>  'https://www.zarinpal.com/pg/StartPay/',
@@ -13,5 +13,5 @@ return [
         'TEST_GATE' =>  'https://sandbox.zarinpal.com/pg/StartPay/',
     ],
 
-    'PAYMENT_CALLBACK' => '/payment/callback',
+    'PAYMENT_CALLBACK' => env('APP_URL').'/payment/callback',
 ];
