52b79484ca2ec843a67adfe6441d25e292426a71
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-04-07 21:42:25 +0330

    update: add fa messages

---
+++ Lang/fa/messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'payment_submited_before'   =>  'این پرداخت قبلا ثبت شده است',
+    'payment_canceld'   =>  'درخواست شما کنسل شده است.',
+    'payment_succeed'    =>  'پرداخت شما با موفقیت انجام شد',
+];
