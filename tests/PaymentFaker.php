6ce4e5ffdc01476923ea8259c0ca54c87c0b6c4e
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-11 16:30:30 +0330

    update: add modelType method

--- tests/PaymentFaker.php
+++ tests/PaymentFaker.php
@@ -5,9 +5,19 @@ final class PaymentFaker
 {
     /**
      * generate fake unique id
+     * @return int
      */
-    public function uniqueId()
+    public function uniqueId(): int
     {
         return fake()->numberBetween(9999, 999999);
+    }
+
+    /**
+     * generate fake model type
+     * @return string
+     */
+    public function modelType(): string
+    {
+        return fake()->lexify("????????") . "\\" . fake()->lexify("????????") . "\\" . fake()->lexify("????????");
     }
 }
