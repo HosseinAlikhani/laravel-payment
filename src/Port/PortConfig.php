dc07cd96c830d6fa36173e8c2ad1eb0f00c3b90a
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-12 00:42:53 +0330

    update: update config

--- src/Port/PortConfig.php
+++ src/Port/PortConfig.php
@@ -57,7 +57,7 @@ final class PortConfig
         try{
             $config = config('payment.'.strtoupper($port));
             $this->apiKey = $config['API_KEY'];
-            $this->callbackUrl = $config['PAYMENT_CALLBACK'] ?? config('payment.PAYMENT_CALLBACK');
+            $this->callbackUrl = $config['PAYMENT_CALLBACK'] ?? config('payment.ADDITIONAL.PAYMENT_CALLBACK');
             if ( isset($config['IS_TEST']) ) {
                 $this->send = $config['TEST_SEND'];
                 $this->gate = $config['TEST_GATE'];
