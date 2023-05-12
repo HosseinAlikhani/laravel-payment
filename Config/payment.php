f6248586c283191697042999f14ed63e8ca210b0
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-05-12 12:59:04 +0330

    update: update payment config

--- Config/payment.php
+++ Config/payment.php
@@ -1,9 +1,14 @@
 <?php
+
+use D3cr33\Payment\Port\ZarinpalPort;
+
 return [
 
     'ZARINPAL' => [
         'ACTIVE'    =>  true,
-        'NAMESPACE' =>  '',
+        'NAMESPACE' =>  ZarinpalPort::class,
+        'ICON_URL'  =>  url('storage/port/zarinpal.png'),
+        'FA_NAME'   =>  'زرین پال',
         'IS_TEST' => true,
 
         'API_KEY'  =>  '1122334455667788998811223344556677',
