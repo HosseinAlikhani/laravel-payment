ada02fc98138fe8bcd02879a7a4c36f37b479fd1
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-06-24 23:09:34 +0330

    update: update en messages

--- Lang/en/messages.php
+++ Lang/en/messages.php
@@ -1,4 +1,6 @@
 <?php
 return [
-
+    'payment_submited_before'   =>  'Payment submited before',
+    'payment_canceld'   =>  'Payment canceld',
+    'payment_succeed'    =>  'Payment succeed',
 ];
