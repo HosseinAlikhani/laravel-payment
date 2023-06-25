d67ef02375612118485b46f370cf3b77342cb769
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-06-25 11:32:34 +0330

    add: add transaction_not_found to en/messages

--- Lang/en/messages.php
+++ Lang/en/messages.php
@@ -3,4 +3,5 @@ return [
     'payment_submited_before'   =>  'Payment submited before',
     'payment_canceld'   =>  'Payment canceld',
     'payment_succeed'    =>  'Payment succeed',
+    'transaction_not_found' =>  'Transaction not found',
 ];
