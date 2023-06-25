9fa0bc7bd538c8b7be48d7c79f565142eecb9120
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-06-25 11:32:49 +0330

    update: add transaction_not_found to fa/messages

--- Lang/fa/messages.php
+++ Lang/fa/messages.php
@@ -3,4 +3,5 @@ return [
     'payment_submited_before'   =>  'این پرداخت قبلا ثبت شده است',
     'payment_canceld'   =>  'درخواست شما کنسل شده است.',
     'payment_succeed'    =>  'پرداخت شما با موفقیت انجام شد',
+    'transaction_not_found' =>  'درخواست معتبر نمی باشد.',
 ];
